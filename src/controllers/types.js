a01494dd2a4ad338022055ee94adf4fb915ce2c4
kinko7 <kion7kion@gmail.com>
2022-06-03 20:16:01 -0300

    BulkTypesDB

--- src/controllers/types.js
+++ src/controllers/types.js
@@ -1,10 +1,28 @@
 const { Types, Pokemons } = require("../db.js");
-const { v4: uuidv4 } = require("uuid");
+const axios = require('axios')
+const { uuid:v4} = require('uuidv4');
 
+const getTypes = async (req,res) => {
+    try{
+        const dbTypes = await Types.findAll({attributes: ['name', 'id']})
+        if(dbTypes.length > 0) return res.json(dbTypes)
+            else{
+                 try{
 
-async function getTypes(req, res, next) {
-
-    
+                    let res = await axios.get('https://pokeapi.co/api/v2/type');
+                    var types = res.data.results.map(el => {return {name: el.name}})
+                    console.log(types)
+                    Types.bulkCreate(types);
+                    return res.json(types);
+                } catch {
+                    (err) => next(err);
+                  }
+              
+            }
+        
+    } catch (error){
+        console.log(error)
+    }
 }
 
 
