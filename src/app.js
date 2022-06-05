c5644cc8b6d9b8768e99857b70f5a63528aa4ba3
kinko7 <kion7kion@gmail.com>
2022-06-05 17:54:30 -0300

    ErrorHandlers/Headers

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cors({ origin: "http://localhost:3000", credentials: true }))
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => { // update to match the domain you will make the request from
+server.use((req, res, next) =>{ // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Origin', "http://localhost:3000");// update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -29,11 +29,13 @@ server.use((req, res, next) => { // update to match the domain you will make the
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {  
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
+
+
 
 module.exports = server;
